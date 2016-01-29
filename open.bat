ea138c3438b91eead5259fa4c598a2d0e59b5c87
sergiorgiraldo <sergiorgiraldo@gmail.com>
2016-01-29 12:38:39 -0200

    batch to easy find a day file

--- open.bat
+++ open.bat
@@ -1,1 +1,9 @@
-REM Receive a date in format YYYYMMDD or as D0(today), D-1(yesterday), etc
+@echo off
+set base=C:\Users\sergio.rodrigues\source\minhasresolucoes.github.io
+set arg1=%1
+set year=%arg1:~0,4%
+set month=%arg1:~4,2%
+set day=%arg1:~6,2%
+
+notepad %base%\%year%\%month%\%arg1%.html
+
