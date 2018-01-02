5a3366558fe54d19bac653dd3b658c784526ee8c
sergiorgiraldo@gmail.com <sergiorgiraldo@gmail.com>
2018-01-02 13:36:07 -0200

    2018

--- console/buildResolucoes/Program.cs
+++ console/buildResolucoes/Program.cs
@@ -9,7 +9,46 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            
+            //buildDays();
+            buildIndex();
+        }
+
+        private static void buildIndex()
+        {
+            var basePath = @"C:\Users\sgiraldo\source\minhasresolucoes.github.io\2018\";
+            var baseYear = 2018;
+            for (int i = 1; i <= 12; i++)
+            {
+                var daysInMonth = DateTime.DaysInMonth(baseYear, i);
+                var fullMonthName = new DateTime(baseYear, i, 1).ToString("MMMM", System.Globalization.CultureInfo.CreateSpecificCulture("pt"));
+
+                var builder = new StringBuilder();
+
+                builder.AppendLine(@"<!DOCTYPE html>");
+                builder.AppendLine(@"<html lang=pt-br>");
+                builder.AppendLine(@"<meta charset=utf-8>");
+
+
+                builder.AppendLine(@"<p><a href=""javascript:history.back(-1);"">Voltar</a><br /></p>");
+                builder.AppendLine("");
+
+                for (int j = 1; j <= daysInMonth; j++)
+                {
+                    var fileName = baseYear + String.Format("{0:00}", i) + String.Format("{0:00}", j) + ".html";
+
+                    var fileName2 = new DateTime(baseYear, i, j).ToString("ddd", new System.Globalization.CultureInfo("pt-BR")) + ", " + String.Format("{0:00}", j) + " de " + fullMonthName + " de " + baseYear;
+
+                    builder.AppendLine("<a href=\"" + fileName + "\">" + fileName2 + "</a><br />");
+                }
+
+                builder.AppendLine(@"</html>");
+
+                File.AppendAllText(basePath + String.Format("{0:00}", i) + @"\index.html", builder.ToString());
+            }
+        }
+
+        private static void buildDays()
+        {
             var basePath = @"C:\Users\sgiraldo\source\minhasresolucoes.github.io\2018\";
             var baseYear = 2018;
             for (int i = 1; i <= 12; i++)
@@ -52,13 +91,13 @@ namespace ConsoleApplication1
                     builder.AppendLine("<div class=\"container\">");
                     builder.AppendLine("<section id=\"main_content\">");
 
-                    builder.AppendLine(@"<p><a href=""index.html""><< " + thisDay.ToString("MMMM", System.Globalization.CultureInfo.CreateSpecificCulture("pt")) +  "</a><br /></p>");
+                    builder.AppendLine(@"<p><a href=""index.html""><< " + thisDay.ToString("MMMM", System.Globalization.CultureInfo.CreateSpecificCulture("pt")) + "</a><br /></p>");
                     builder.AppendLine("[ ] Rezar uma vez ao dia <br /> <br />");
                     builder.AppendLine("[ ] Família <br /> <br />");
                     builder.AppendLine("[ ] Emagrecer <br /> <br />");
                     builder.AppendLine("[ ] Tese de Mestrado <br /> <br />");
 
-                    if (fileNameYesterday != "" || fileNameTomorrow!= "")
+                    if (fileNameYesterday != "" || fileNameTomorrow != "")
                     {
                         builder.AppendLine(@"<p>");
                         if (fileNameYesterday != "")
@@ -66,7 +105,7 @@ namespace ConsoleApplication1
                             if (thisDay.Month != yesterday.Month)
                                 builder.AppendLine(string.Format("<a href=\"..\\{0:00}\\{1}\">ant.</a>", yesterday.Month, fileNameYesterday));
                             else
-                                builder.AppendLine("<a href=\""  + fileNameYesterday + "\">ant.</a>");
+                                builder.AppendLine("<a href=\"" + fileNameYesterday + "\">ant.</a>");
                         }
                         if (fileNameTomorrow != "")
                         {
@@ -91,39 +130,6 @@ namespace ConsoleApplication1
 
                 }
             }
-            
-            /*
-            var basePath = @"C:\Users\Sérgio\Source\minhasresolucoes.github.io\2017\";
-            var baseYear = 2017;
-            for (int i = 1; i <= 12; i++)
-			{
-                var daysInMonth = DateTime.DaysInMonth(baseYear, i);
-                var fullMonthName = new DateTime(baseYear, i, 1).ToString("MMMM", System.Globalization.CultureInfo.CreateSpecificCulture("pt"));
-
-                var builder = new StringBuilder();
-
-                builder.AppendLine(@"<!DOCTYPE html>");
-                builder.AppendLine(@"<html lang=pt-br>");
-                builder.AppendLine(@"<meta charset=utf-8>");
-
-
-                builder.AppendLine(@"<p><a href=""javascript:history.back(-1);"">Voltar</a><br /></p>");
-                builder.AppendLine("");
-
-                for (int j = 1; j <= daysInMonth; j++)
-                {
-                    var fileName = baseYear + String.Format("{0:00}", i) + String.Format("{0:00}", j) + ".html";
-
-                    var fileName2 = new DateTime(baseYear, i, j).ToString("ddd", new System.Globalization.CultureInfo("pt-BR")) + ", " + String.Format("{0:00}", j) + " de " + fullMonthName + " de " + baseYear;
-
-                    builder.AppendLine("<a href=\"" + fileName+ "\">" + fileName2 + "</a><br />");
-                }
-
-                builder.AppendLine(@"</html>");
-
-                File.AppendAllText(basePath + String.Format("{0:00}", i) + @"\index.html", builder.ToString());
-			}
-             */ 
         }
     }
 }
